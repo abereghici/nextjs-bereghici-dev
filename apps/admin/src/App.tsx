47b8f4e905ffe744d68d7aa8f6cf8f79683406ca
Alexandru Bereghici <abereghici@tripadvisor.com>
2021-05-17 19:35:32 +0300

    Fix lint staged command

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         }}
         onClick={() => {
           // eslint-disable-next-line no-console
-          console.log('Test!');
+          console.log('Test');
         }}
       >
         Test button
