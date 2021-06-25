bf8b555179ff54f14be6de9ab7df17c991d60a20
Alexandru Bereghici <abereghici@tripadvisor.com>
2021-06-25 16:33:19 +0300

    netlify deploy nextjs

--- apps/blog/pages/index.tsx
+++ apps/blog/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome <a href="https://nextjs.org">!</a>
         </h1>
 
         <p className={styles.description}>
