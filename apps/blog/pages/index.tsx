e6b775a16b4ef2cd62facfeff554ceef38bba979
Alexandru Bereghici <abereghici@users.noreply.github.com>
2021-06-26 17:46:06 +0300

    netlify deploy nextjs (#29)

--- apps/blog/pages/index.tsx
+++ apps/blog/pages/index.tsx
@@ -46,7 +46,7 @@ export default function Home() {
           >
             <h2>Deploy &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Instantly deploy your Next.js site to a public URL with Vercel
             </p>
           </a>
         </div>
