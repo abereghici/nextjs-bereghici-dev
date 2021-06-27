e745e05997a36c555986709a65c583283991d8ef
Alexandru Bereghici <abereghici@tripadvisor.com>
2021-06-27 23:39:02 +0300

    Test netlify

--- apps/blog/pages/index.tsx
+++ apps/blog/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          <a href="https://nextjs.org">Welcome</a>
+          <a href="https://nextjs.org">Welcome!</a>
         </h1>
 
         <p className={styles.description}>
